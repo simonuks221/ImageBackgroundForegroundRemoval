e5f32b2458ea30e2cddc4dd36ccabea10134ca54
simonuks221 <simonasriauka@gmail.com>
2021-06-06 18:24:22 +0300

    Initial

---
+++ image-changer/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className = 'text-light'>Lalalala</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
